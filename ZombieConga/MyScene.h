5d74394ceb302604aa861a1b095e1202d6f4efb0
Aaron Vasquez <aaronvasquez1@gmail.com>
2014-03-08 16:19:48 -0500

    Initial Commit

---
+++ ZombieConga/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  ZombieConga
+//
+
+//  Copyright (c) 2014 Spud Cannon LLC. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ ZombieConga/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZombieConga
+//
+//  Created by Aaron Vasquez on 3/8/14.
+//  Copyright (c) 2014 Spud Cannon LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
