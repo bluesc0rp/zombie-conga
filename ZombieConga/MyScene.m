730d24a316d0e52a7e17fc639c40866019c3d913
Aaron Vasquez <aaronvasquez1@gmail.com>
2014-03-10 21:58:39 -0400

    made code resemble tutorial so it is easier to follow

--- ZombieConga/MyScene.m
+++ ZombieConga/MyScene.m
@@ -8,163 +8,216 @@
 
 #import "MyScene.h"
 
-// math helpers
-static inline CGPoint CGPointAdd(const CGPoint a, const CGPoint b){
+static inline CGPoint CGPointAdd(const CGPoint a,
+                                 const CGPoint b)
+{
     return CGPointMake(a.x + b.x, a.y + b.y);
 }
-static inline CGPoint CGPointSubtract(const CGPoint a, const CGPoint b){
+
+static inline CGPoint CGPointSubtract(const CGPoint a,
+                                      const CGPoint b)
+{
     return CGPointMake(a.x - b.x, a.y - b.y);
 }
-static inline CGPoint CGPointMultiplyScalar(const CGPoint a, const CGFloat b){
+
+static inline CGPoint CGPointMultiplyScalar(const CGPoint a,
+                                            const CGFloat b)
+{
     return CGPointMake(a.x * b, a.y * b);
 }
-static inline CGFloat CGPointLength(const CGPoint a) {
+
+static inline CGFloat CGPointLength(const CGPoint a)
+{
     return sqrtf(a.x * a.x + a.y * a.y);
 }
-static inline CGPoint CGPointNormalize(const CGPoint a) {
+
+static inline CGPoint CGPointNormalize(const CGPoint a)
+{
     CGFloat length = CGPointLength(a);
     return CGPointMake(a.x / length, a.y / length);
 }
-static inline CGFloat CGPointToAngle(const CGPoint a) {
+
+static inline CGFloat CGPointToAngle(const CGPoint a)
+{
     return atan2f(a.y, a.x);
 }
 
-static CGFloat const kZombieMovePointsPerSec = 120;
+static inline CGFloat ScalarSign(CGFloat a)
+{
+    return a >= 0 ? 1 : -1;
+}
 
-@interface MyScene()
-@property (weak, nonatomic) SKSpriteNode *zombie; // views should be weak because self.subviews points to them
-@property (weak, nonatomic) SKSpriteNode *bg;
-@property (nonatomic) NSTimeInterval lastUpdateTime;
-@property (nonatomic) NSTimeInterval deltaTime;
-@property (nonatomic) CGPoint velocity; // point representing a vector
-@property (nonatomic) CGPoint lastTouched;
-@end
+// Returns shortest angle between two angles,
+// between -M_PI and M_PI
+static inline CGFloat ScalarShortestAngleBetween(
+                                                 const CGFloat a, const CGFloat b)
+{
+    CGFloat difference = b - a;
+    CGFloat angle = fmodf(difference, M_PI * 2);
+    if (angle >= M_PI) {
+        angle -= M_PI * 2;
+    }
+    return angle;
+}
+
+static const float ZOMBIE_MOVE_POINTS_PER_SEC = 120.0;
+static const float ZOMBIE_ROTATE_RADIANS_PER_SEC = 4 * M_PI;
 
 @implementation MyScene
-
-# pragma mark - lazy getters and setters
-
-- (SKSpriteNode *)zombie
 {
-    if (!_zombie) {
+    SKSpriteNode *_zombie;
+    NSTimeInterval _lastUpdateTime;
+    NSTimeInterval _dt;
+    CGPoint _velocity;
+    CGPoint _lastTouchLocation;
+}
+
+-(id)initWithSize:(CGSize)size
+{
+    if (self = [super initWithSize:size]) {
+        self.backgroundColor = [SKColor whiteColor];
+        SKSpriteNode *bg =
+        [SKSpriteNode spriteNodeWithImageNamed:@"background"];
+        bg.position =
+        CGPointMake(self.size.width/2, self.size.height/2);
+        bg.position =
+        CGPointMake(self.size.width / 2, self.size.height / 2);
+        bg.anchorPoint = CGPointMake(0.5, 0.5); // same as default
+        //bg.zRotation = M_PI / 8;
+        [self addChild:bg];
+        
         _zombie = [SKSpriteNode spriteNodeWithImageNamed:@"zombie1"];
         _zombie.position = CGPointMake(100, 100);
-//        [_zombie setScale:2.0f];
-    }
-    return _zombie;
-}
-
-- (SKSpriteNode *)bg
-{
-    if (!_bg) {
-        _bg = [SKSpriteNode spriteNodeWithImageNamed:@"background"];
-        _bg.position = CGPointMake(self.size.width/2, self.size.height/2);
-    }
-    return _bg;
-}
-
-# pragma mark - view lifecycle
-
--(id)initWithSize:(CGSize)size {    
-    if (self = [super initWithSize:size]) {
-        [self setup];
+        [self addChild:_zombie];
+        
+        //[_zombie setScale:2.0]; // SKNode method
+        
     }
     return self;
 }
 
-# pragma mark - touch handlers
-
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
-{
-    [self handleTouch:[touches anyObject]];
-}
-
--(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
-{
-    [self handleTouch:[touches anyObject]];
-}
-
--(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
-{
-    [self handleTouch:[touches anyObject]];
-}
-
-# pragma mark - SpriteKit frame updates
+//// Gesture recognizer example
+//// Uncomment this, and comment the touchesBegan/Moved/Ended methods to test
+//- (void)didMoveToView:(SKView *)view
+//{
+//  UITapGestureRecognizer * tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap:)];
+//  [self.view addGestureRecognizer:tapRecognizer];
+//}
+//
+//- (void)handleTap:(UITapGestureRecognizer *)recognizer {
+//  CGPoint touchLocation = [recognizer locationInView:self.view];
+//  touchLocation = [self convertPointFromView:touchLocation];
+//  [self moveZombieToward:touchLocation];
+//}
 
 - (void)update:(NSTimeInterval)currentTime
 {
-    // update timers
-    self.deltaTime = self.lastUpdateTime ? currentTime-self.lastUpdateTime : 0;
-    self.lastUpdateTime = currentTime;
+    if (_lastUpdateTime) {
+        _dt = currentTime - _lastUpdateTime;
+    } else {
+        _dt = 0;
+    }
+    _lastUpdateTime = currentTime;
     
-    // move zombay
-    if (![self stopZombie]) {
-        [self moveSprite:self.zombie withVelocity:self.velocity];
+    CGPoint offset = CGPointSubtract(_lastTouchLocation, _zombie.position);
+    float distance = CGPointLength(offset);
+    if (distance < ZOMBIE_MOVE_POINTS_PER_SEC * _dt) {
+        _zombie.position = _lastTouchLocation;
+        _velocity = CGPointZero;
+    } else {
+        [self moveSprite:_zombie velocity:_velocity];
+        [self boundsCheckPlayer];
+        [self rotateSprite:_zombie toFace:_velocity rotateRadiansPerSec:ZOMBIE_ROTATE_RADIANS_PER_SEC];
     }
 }
 
-# pragma mark - helpers
-
-- (void)setup
+- (void)moveSprite:(SKSpriteNode *)sprite
+          velocity:(CGPoint)velocity
 {
-    self.backgroundColor = [SKColor whiteColor];
-    [self addChild:self.bg];
-    [self addChild:self.zombie];
-}
-
-- (void)handleTouch:(UITouch *)touch
-{
-    CGPoint touchLocation = [touch locationInNode:self];
-    self.lastTouched = touchLocation;
-    [self moveZombieToward:touchLocation];
-}
-
-- (void)moveSprite:(SKSpriteNode *)sprite withVelocity:(CGPoint)velocity
-{
-    // position = dx/dt * deltatime
-    CGPoint amountToMove = CGPointMultiplyScalar(velocity, self.deltaTime);
+    // 1
+    CGPoint amountToMove = CGPointMultiplyScalar(velocity, _dt);
+    
+    // 2
     sprite.position = CGPointAdd(sprite.position, amountToMove);
-    [self rotatoSprite:sprite toFace:velocity];
-    [self boundsCheckPlayer];
-
-}
-
-- (void)rotatoSprite:(SKSpriteNode *)sprite toFace:(CGPoint)direction
-{
-    sprite.zRotation = CGPointToAngle(direction);
 }
 
 - (void)moveZombieToward:(CGPoint)location
 {
-    CGPoint offset = CGPointSubtract(location, self.zombie.position);;// direction with rando velocity
-    CGPoint direction = CGPointNormalize(offset); // normalized vector...use unit of 1
-    self.velocity = CGPointMultiplyScalar(direction, kZombieMovePointsPerSec); // apply velocity scaler
+    _lastTouchLocation = location;
+    CGPoint offset = CGPointSubtract(location, _zombie.position);
+    
+    CGPoint direction = CGPointNormalize(offset);
+    _velocity = CGPointMultiplyScalar(direction, ZOMBIE_MOVE_POINTS_PER_SEC);
+    
 }
 
-- (BOOL)stopZombie
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    CGPoint distToStop = CGPointSubtract(self.lastTouched, self.zombie.position);
-    CGPoint zombieMove = CGPointMultiplyScalar(self.velocity, self.deltaTime);
-    CGFloat toStopLength = CGPointLength(distToStop);
-    CGFloat toMoveLength = CGPointLength(zombieMove);
-    if (toStopLength < toMoveLength) {
-        self.velocity = CGPointZero;
-        self.zombie.position = self.lastTouched;
-    }
-    return NO;
+    UITouch *touch = [touches anyObject];
+    CGPoint touchLocation = [touch locationInNode:self.scene];
+    [self moveZombieToward:touchLocation];
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint touchLocation = [touch locationInNode:self.scene];
+    [self moveZombieToward:touchLocation];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint touchLocation = [touch locationInNode:self.scene];
+    [self moveZombieToward:touchLocation];
 }
 
 - (void)boundsCheckPlayer
 {
-    CGPoint newVelocity = self.velocity;
-    if (self.zombie.position.x>=self.size.width || self.zombie.position.x<=0) {
+    // 1
+    CGPoint newPosition = _zombie.position;
+    CGPoint newVelocity = _velocity;
+    
+    // 2
+    CGPoint bottomLeft = CGPointZero;
+    CGPoint topRight = CGPointMake(self.size.width,
+                                   self.size.height);
+    
+    // 3
+    if (newPosition.x <= bottomLeft.x) {
+        newPosition.x = bottomLeft.x;
         newVelocity.x = -newVelocity.x;
     }
-    if (self.zombie.position.y>=self.size.height || self.zombie.position.y<=0) {
+    if (newPosition.x >= topRight.x) {
+        newPosition.x = topRight.x;
+        newVelocity.x = -newVelocity.x;
+    }
+    if (newPosition.y <= bottomLeft.y) {
+        newPosition.y = bottomLeft.y;
         newVelocity.y = -newVelocity.y;
     }
-    self.velocity = newVelocity;
-    // also hold zombie to position?
+    if (newPosition.y >= topRight.y) {
+        newPosition.y = topRight.y;
+        newVelocity.y = -newVelocity.y;
+    }
+    
+    // 4
+    _zombie.position = newPosition;
+    _velocity = newVelocity;
 }
+
+- (void)rotateSprite:(SKSpriteNode *)sprite
+              toFace:(CGPoint)velocity
+ rotateRadiansPerSec:(CGFloat)rotateRadiansPerSec
+{
+    float targetAngle = CGPointToAngle(velocity);
+    float shortest = ScalarShortestAngleBetween(sprite.zRotation, targetAngle);
+    float amtToRotate = rotateRadiansPerSec * _dt;
+    if (ABS(shortest) < amtToRotate) {
+        amtToRotate = ABS(shortest);
+    }
+    sprite.zRotation += ScalarSign(shortest) * amtToRotate;
+}
+
 
 @end
